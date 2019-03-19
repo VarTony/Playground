77e54e004e3ea045af1bb5035ce29e1f96a60275
VarTony <littlesalo3@gmail.com>
2019-03-19 21:48:20 +0300

    027

--- JS/DOM/frameworks\library/React/React+nodejs/view/src/components/Canvas.jsx
+++ JS/DOM/frameworks\library/React/React+nodejs/view/src/components/Canvas.jsx
@@ -1,11 +1,39 @@
 import React, {Component} from 'react';
+// import * as CAV from './Controlers'; //controlers and them values
+import  Controlers from './Controlers';
 
+class Canvas extends Component   {
+	// moveBrush (e) {
+	// 	this.canvas.onmousemove = e => {		
+	// 		let x = e.offsetX;
+	// 		let y = e.offsetY;
+	// 		this.ctx.fillStyle = this.color.value;
+	// 		this.ctx.fillRect(x-5, y-5, 10, 10);
+	// 			}
 
-class Canvas extends Component {
+	// stopMoveBrush = () => this.canvas.onmousemove = null;
+	// }
+
+	 componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+
+    canvas.onmousedown = e => {
+   		canvas.onmousemove = e => {		
+				let x = e.offsetX;
+				let y = e.offsetY;
+				ctx.fillStyle = this.state.color;
+				ctx.fillRect(x-5, y-5, 10, 10);
+		}
+	canvas.onmouseup = () => canvas.onmousemove = null;
+	}
+
+  }
+
 
 	render () {
 		return (
-			<canvas id='canvas'></canvas>
+			<canvas ref="canvas" id='canvas' width={1000} height={650}></canvas>
 			);
 	}
 }
--- JS/DOM/frameworks\library/React/React+nodejs/view/src/components/Paint.jsx
+++ JS/DOM/frameworks\library/React/React+nodejs/view/src/components/Paint.jsx
@@ -2,14 +2,24 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import Canvas from './Canvas';
 import Controlers from './Controlers';
+// import PaintScript from '../scripts/Paint.js';
 
 
 class Paint extends Component {
 
+	giveValues(e) {
+
+		const { name, value } = e.target;
+        this.setState({
+          [name]: value
+        });
+
+	}
+
 	render () {
 		return (
 			<div id='paint'>
-				<Controlers/>
+				<Controlers />
 				<Canvas/>
 			 </div>
 			);
