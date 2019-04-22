dffbccb45794209b7b68fcddf01ced787d737ad5
VarTony <littlesalo3@gmail.com>
2019-04-22 20:04:08 +0300

    047

--- JS/NODEJS/WEB/server004/view/server004/components/contact_field/Contact.jsx
+++ JS/NODEJS/WEB/server004/view/server004/components/contact_field/Contact.jsx
@@ -6,7 +6,7 @@ class Contact extends React.Component {
 		super(props);
 
 		this.state = {
-			visibility : 'hidden',
+			visibility : 'hidden right-side',
 			visibilityBtn : 'hidden btns'
 		}
 
@@ -14,7 +14,7 @@ class Contact extends React.Component {
 
 	changeVisibility () {
 		this.setState({
-			visibility : this.state.visibility ===  'hidden'? 'visible' : 'hidden',
+			visibility : this.state.visibility ===  'hidden right-side'? 'visible right-side' : 'hidden right-side',
 			visibilityBtn :  this.state.visibilityBtn === 'hidden btns' ? 'visible btns': 'hidden btns'
 		});
 	}
@@ -22,15 +22,16 @@ class Contact extends React.Component {
 	render() {
 		console.log(this.props.data.numberPhone);
 		return(
-			<div id='Contact'> 
+			<div id='Contact' className='contacts'> 
 				<div className='left-side'>
 					<img src={this.props.data.img} onClick={e => this.changeVisibility()} id='GIDOnContact'/>
 					<button className={this.state.visibilityBtn} id='editBtn' >Edit</button>
 				</div>
 
-				<div className='right-side' className={this.state.visibility}>
+				<div className={this.state.visibility}>
 					<h3> Id: {this.props.data.id}</h3>
-					<h3> Name: {this.props.data.name} Lastname: {this.props.data.lastname}</h3>
+					<h3> Name: {this.props.data.name}</h3>
+					<h3> Lastname: {this.props.data.lastname}</h3>
 					<h3>Number phone: {this.props.data.number_phone} Email: {this.props.data.email}</h3>
 					<button className='btns' id='deleteBtn'>Delete</button>
 				</div>
