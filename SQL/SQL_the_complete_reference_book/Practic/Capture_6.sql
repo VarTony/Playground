c9bdbf1928195e8ad9d05f125c46603c3eccdfab
VarTony <littlesalo3@gmail.com>
2019-10-09 07:05:05 +0300

    094.5

--- SQL/SQL_the_complete_reference_book/Practic/Capture_6.sql
+++ SQL/SQL_the_complete_reference_book/Practic/Capture_6.sql
@@ -3,7 +3,7 @@
 --Вычисляемые столбцы.
 
 --Example-1
--Увеличить плановые продажи на 3% от фактических для каждого служаща=е=его.
+--Увеличить плановые продажи на 3% от фактических для каждого служаща=е=его.
 SELECT name, quota, (quota + (.03 * sales)) 
     FROM salesreps;
 
@@ -12,4 +12,55 @@ SELECT name, quota, (quota + (.03 * sales))
 --Повышение удобочитаемости при выводе данных с помощью использования констант.
 SELECT city, 'has sales of', sales
     FROM offices;
+
+
+--Example-3
+--Distinct
+SELECT DISTINCT mgr FROM offices;
+
+--Example-4
+--BETWEEN
+
+SELECT order_num, amount 
+    FROM orders 
+    WHERE amount BETWEEN 25000  AND 34999;
+
+
+--Example-5
+--NOT BETWEEN
+
+SELECT name, sales, quota 
+    FROM salesreps
+    WHERE sales NOT BETWEEN (0.8 * quota) AND (1.2 * quota);
+
+
+--Example-6
+-- IN
+
+SELECT name, quota, sales
+    FROM salesreps
+    WHERE rep_office IN (4, 8, 15, 11, 16, 23, 42);
+
+
+--Example-7
+--LIKE
+
+SELECT name, sales 
+    FROM salesreps 
+    WHERE name LIKE 'N%';
+
+
+--Example-8
+-- IS NULL
+
+SELECT name
+    FROM salesreps 
+    WHERE rep_office IS NULL;
+
+
+
+
+
+
+
 
