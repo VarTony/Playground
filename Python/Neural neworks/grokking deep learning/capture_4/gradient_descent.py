d0ec38bfde9b937f459159104dd4a78b2f1000dd
Anion <littlesalo3@gmail.com>
2021-02-10 18:54:34 +0300

    127

--- Python/Neural neworks/grokking deep learning/capture_4/gradient_descent.py
+++ Python/Neural neworks/grokking deep learning/capture_4/gradient_descent.py
@@ -2,7 +2,7 @@ weight = 0.5
 goal_pred = 0.8
 input = 0.5
 
-for iteration in range(1000):
+for iteration in range(10000000):
   pred = input * weight
   error = (pred - goal_pred) ** 2
   direction_and_amount = (pred - goal_pred) * input #Main 
