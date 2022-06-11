2207cc0c7670f8d1bd8c1cb6b7d1ca2da122dcb0
Anion <littlesalo3@gmail.com>
2022-06-12 00:59:46 +0300

    137

--- JS/PURE_JS/MATH/Some functions/squareEquation.js
+++ JS/PURE_JS/MATH/Some functions/squareEquation.js
@@ -1,6 +1,6 @@
 /*
-Процедура для нахождения корней квадратного уравнения, не работает с отрицательными коэфициэнтами 'a', 
-так как функционирует в диапозоне вещественных(действительнаых) чисел.
+Процедура для нахождения корней квадратного уравнения, не работает с отрицательными коэффициентами 'a', 
+так как функционирует в диапазоне вещественных (действительных) чисел.
 
 Используется метод полного квадрата.
 
@@ -18,29 +18,35 @@ a | x^2 +/- bx + (b/2)^2 = +/- с + (b/2)^2
     x1 =   (_/(c + (b/2)^2) +/- (b/2)) / a
     x2 =  (-_/(c + (b/2)^2) +/- (b/2)) / a
 
-
 */
 
 
 const squareEquation = (a, b, c, fix = 1) => {
+    let x1, x2;
   
     if(a < 0) return 'Эта версия не работает с мнимыми числами';
-  
     if(a === 0) return 0;
   
-    const bDivided = Math.abs((b/2));
-    
-    const square = (Math.sqrt((-c) + (bDivided)**2));
-    
-    const x1 =  ((square + bDivided) / a).toFixed(fix);
-    const x2 =  ((-square + bDivided) / a).toFixed(fix);
+    const bDivided = (b/2);
+    const rightSide = (c * -1) + Math.abs(bDivided)**2;
   
+    if(rightSide < 0) x1 = x2 = 'i'; // Если правая сторона меньше 0, то ее корни являются мнимыми числами
+    
+    else {
+      const square = Math.sqrt(rightSide);
+  
+      x1 =  ((square + (-bDivided)) / a).toFixed(fix);
+      x2 =  ((-square + (-bDivided)) / a).toFixed(fix);
+    }
+      
     return {
       x1, 
       x2, 
-      initialForm: `Initial form this equation: (x + (${-x1}))(x + (${-x2}))`
+      initialForm: `Initial form this equation: (x + (${-x1? -x1: x1}))(x + (${-x2? -x2: x2}))`
     }
   } 
   
   
-  console.log(squareEquation(1, -15, 34)); // Initial form this equation: (x + (-12.2))(x + (-2.8))
+  console.table(squareEquation(1, 11, 34)); // Initial form this equation: (x + (i))(x + (i))
+  
+  console.table(squareEquation(1, -15, 34)); // Initial form this equation: (x + (-12.2))(x + (-2.8))
