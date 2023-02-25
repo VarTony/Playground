4ce845a10449142d6d32be75299af18ce37f2add
Anion <littlesalo3@gmail.com>
2023-02-25 21:09:20 +0300

    154

--- JS/PURE_JS/MATH/Linear Algebra/basic_methods.js
+++ JS/PURE_JS/MATH/Linear Algebra/basic_methods.js
@@ -7,20 +7,59 @@ import {
 
 
 
+/* Транспонирование вектора (Внутреняя функция)
+    Сигнатура:
+    v - [ 𐤀a ϵ R| a1, ..., an ]: Вектор
+    |
+    |___/ Opertation ---->  v := [ 1,2,3 ] -> [ [1],[2],[3] ] 
+*/
+const vectorT = v => v.reduce((column, a) => { 
+    column = [...column, [a]];
+    return column;
+  }, []);
+
+
+/* Транспонирование матрицы (Внутреняя функция)
+    Сигнатура:
+    A - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица
+    |
+    |___/ Opertation ---->  A := [ [1,2,3]    [ [1,4],
+                                   [4,5,6] ] -> [2,5],
+                                                [3,6] ] 
+
+*/
+const matrixT = (A, newMatrix = [], iOfColumn = 0) => {
+    const newColumn = [];
+    A.forEach(v => newColumn.push(v[iOfColumn]))
+          
+    return iOfColumn === A[0].length 
+     ? newMatrix
+     : transposited([...newMatrix, newColumn], ++iOfColumn)
+  }
+
+
+/* Транспонирование матрицы (Внешний деспетчер)
+    Сигнатура:
+    A - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица
+*/
+const matrixTransposition = A => detectorMatrixOrVector(A) === 'vector'
+    ? vectorT(A)
+    : matrixT(A)
+
 
 
 /* Умножение вектора на число
- Сигнатура:
- V - [ 𐤀a ϵ R| a1, ..., an ]: Вектор
- n - (n ϵ R): Множитель
+    Сигнатура:
+    V - [ 𐤀a ϵ R| a1, ..., an ]: Вектор
+    n - (n ϵ R): Множитель
 */
 const vectorToNum = (V, n) => V.map( a => a * n);
 
 
 /* Умножение матрицы на число
- Сигнатура:
- A - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица
- n - (n ϵ R): Множитель
+    Сигнатура:
+    A - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица
+    n - (n ϵ R): Множитель
 |
 |___/ Opertation ----> ( A * n )
 
@@ -29,13 +68,27 @@ const matrixToNum = (A, n) => A.map( vector => vectorToNum(vector, n) );
 
 
 /* Сложение векторов
+    Сигнатура:
     v1 - [ 𐤀a ϵ R| a1, ..., an ]: Вектор 1
     v2 - [ 𐤀a ϵ R| a1, ..., an ]: Вектор 2
+    |
+    |___/ Opertation ----> ( V1 + V2 )
 */
 const vectorSum = (v1, v2) => v1.map((a, i) => a + v2[i]) 
 
 
-/* Умножение векторов 
+/* Разница векторов
+    Сигнатура:
+    v1 - [ 𐤀a ϵ R| a1, ..., an ]: Вектор 1
+    v2 - [ 𐤀a ϵ R| a1, ..., an ]: Вектор 2
+    |
+    |___/ Opertation ----> ( V1 - V2 )
+*/
+const vectorDiff = (v1, v2) => v1.map((a, i) => a - v2[i]) 
+
+
+/* Умножение векторов
+    Сигнатура: 
     v1 - [ 𐤀a ϵ R| a1, ..., an ]: Вектор 1
     v2 - [ 𐤀a ϵ R| a1, ..., an ]: Вектор 2
     |
@@ -50,6 +103,7 @@ const vectorToVector = (v1, v2) => {
 
 
 /* Сложение матриц
+    Сигнатура:
     A1 - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица 1
     A2 - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица 2
     |
@@ -69,7 +123,30 @@ const matrixSum = (A1, A2) => {
 }
 
 
+
+/* Разница матриц
+    Сигнатура:
+    A1 - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица 1
+    A2 - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} & 𐤀A := {v1, .... vn }  ]: Матрица 2
+    |
+    |___/ Opertation ----> ( A1 - A2 | V1 - V2 ) 
+*/
+const matrixDiff = (A1, A2) => {
+    const validatedSize = comparisonTwoLength(A1, A2, 'Матрицы'); //! Валидацию суммы и разницы предпочтительно вынести на уровень выше
+    const validatedType = comparisonTwoMatrixType(A1, A2);
+    const detectType = detectorMatrixOrVector(A1);
+
+    if(!validatedSize.result) return validatedSize.message;
+    if(!validatedType.result) return validatedType.message;
+    if(detectType === 'vector') return vectorDiff(A1, A2);
+
+    return A1.map( (v, i) => vectorDiff(v, A2[i]))
+}
+
+
+
 /* Умножение матриц
+    Сигнатура:
     A1 - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} ]: Матрица 1
     A2 - [ v ϵ A, (𐤀a ϵ v: a ϵ R)| 𐤀v := {a1, ..., an} ]: Матрица 1
     |
@@ -98,10 +175,13 @@ const matrixToMatrix = (A1, A2) => {
 
 
   exports [ 
+    matrixTransposition,
     vectorToNum,
     matrixToNum,
     vectorSum,
+    vectorDiff,
     matrixSum,
+    matrixDiff,
     vectorToVector,
     matrixToMatrix 
 ];
