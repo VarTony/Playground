0b9f42f5b84dcc463dd68f716d946c4ed84b45a8
Anion <littlesalo3@gmail.com>
2023-03-01 00:04:01 +0300

    156

---
+++ JS/PURE_JS/MATH/Algebra/Enother Functions/factorial.js
@@ -0,0 +1,34 @@
+
+
+// Стандартная версия через итерацию с сохранением промежуточного состояния
+const factorial = n => {
+    let result = i = 1;
+    
+    while(i <= n) {
+      result *= i
+      i++;
+    }
+    return result;
+}
+  
+
+/* Быстрая версия реализованная через использование каскадной рекурсии
+    основана на идеи, того, что близкие по разрядности двоичные числа требуют меньше
+    вычислительных ресурсов для произведения их между собой.
+*/
+const fastFact = n => {
+    if(n === 0) return 1;
+    if(n === 1 || n === 2) return n;
+    
+    const facTree = (left, right) => {
+      if(left > right) return 1;
+      if(right === left) return right;
+      if(right - left === 1) return left * right;
+      let mian = Math.round((left + right) / 2);
+  
+      return facTree(left, mian) * facTree(mian + 1, right) 
+  }
+    return facTree(2, n);
+  }
+  
+  
