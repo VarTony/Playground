9a2e9fcb719a20eb252b2ec9a51bfb6f07de29e1
Anion <littlesalo3@gmail.com>
2023-04-06 22:34:04 +0300

    Little refactoring of polynomial`s parser

--- JS/PURE_JS/MATH/Algebra/Polynomials/Parsers/polynomParser.js
+++ JS/PURE_JS/MATH/Algebra/Polynomials/Parsers/polynomParser.js
@@ -6,8 +6,10 @@ import { monomParser } from "./exports";
      polynom - String -> Полином в виде строки
 */
 const polyParser = polynom => {
-    const p = polynom.trim().toLowerCase();
-    const monomsList = p.replace(/[\+-](\d[a-z]\^[-]?\d)*/g, ' $1').split(' ');
+    const neatPolynom = polynom.trim().toLowerCase();
+    const monomsList = neatPolynom
+      .replace(/[\+-](\d[a-z]\^[-]?\d)*/g, ' $1')
+      .split(' ');
     const ParsedMonomsList = monomsList.map(monom => monomParser(monom));
   
     return ParsedMonomsList;
