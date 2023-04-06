88a6c4d8ef296868e78b65612cf91739c2de4c34
Anion <littlesalo3@gmail.com>
2023-04-06 14:24:31 +0300

    Added descriptions for new parser`s` functions

--- JS/PURE_JS/MATH/Algebra/Polynomials/Parsers/monomParser.js
+++ JS/PURE_JS/MATH/Algebra/Polynomials/Parsers/monomParser.js
@@ -1,22 +1,36 @@
+/* Извлекатель степени из одночлена (Работает с только целыми степенями)
+    Сигнатура:
+     monom - String -> Одночлен записанный строкой (Пример: '-15biba^-25')
+
+     return - Object -> Пример: { b: 2, i: 1, a: -25 }
+      
+P.S Складывает степени одинаковых переменных
+*/
 const getMonomsPowers = monom => monom
-    .replace(/([\+-]?([a-z]\^\-){1}\d+)/g, ' $1')
-    .split(' ')
-    .reduce((powersMap, chunk) => {
-      if (chunk.includes('^')) {
-        const value = +chunk.slice(2);
-        const key = chunk[0];
+  .replace(/([\+-]?([a-z]\^\-){1}\d+)/g, ' $1')
+  .split(' ')
+  .reduce((powersMap, chunk) => {
+    if (chunk.includes('^')) {
+      const value = +chunk.slice(2);
+      const key = chunk[0];
 
-        powersMap[key] = value;
-      }
-      else chunk.replace(/[\+-]|\d/g, '')
-        .split('')
-        .forEach(key => powersMap[key] ? powersMap[key] += 1 : powersMap[key] = 1);
+      powersMap[key] = value;
+    }
+    else chunk.replace(/[\+-]|\d/g, '')
+      .split('')
+      .forEach(key => powersMap[key] ? powersMap[key] += 1 : powersMap[key] = 1);
 
-      return powersMap;
-    }, {})
+    return powersMap;
+  }, {})
   
 
 
+/* Извлекатель множителя одночлена
+  Сигнатура:
+    monom - String -> Одночлен записанный строкой (Пример: '15b^5')
+
+    return - Number -> Пример: coefficient: 15
+*/
 const getMonomCoefficient = monom => {
   const coefficient = monom.match(/^([\+-]?\d*)/g)[0];
   
@@ -29,15 +43,19 @@ const getMonomCoefficient = monom => {
 }
 
 
+/* Извлекатель переменных из одночлена
+    Сигнатура:
+     monom - String -> Одночлен записанный строкой (Пример: '-ia')
+
+     return - String -> Пример: symbols: 'a'
+*/
 const getVariablesFromPowerKeys = powers => {
   const isVars =  Object.keys(powers).join(' ');
   return isVars !== '' ? isVars : 'EMPTY';
 }
 
 
-
-
-/* (Beta) Парсер для одночленов (Работает с целыми степенями)
+/* (Beta) Парсер для одночленов (Работает с только целыми степенями)
     Сигнатура:
      monom - String -> Одночлен записанный строкой (Пример: '-15biba^-25')
 
@@ -48,7 +66,6 @@ const getVariablesFromPowerKeys = powers => {
         powers: { b: 2, i: 1, a: -25 }
       }
 P.S Складывает степени, если переменные одинаковы
-
 */
 const monomParser = monom => {
   const symbols = monom.replace(/[\+-]|\d|\^/g, '');
