0a9310fe5ac70cc252644b72ec2740e5b48001d5
Anion <littlesalo3@gmail.com>
2023-04-08 17:30:05 +0300

    Rewrited polynomial`s sumator

--- JS/PURE_JS/MATH/Algebra/Polynomials/basic_methods.js
+++ JS/PURE_JS/MATH/Algebra/Polynomials/basic_methods.js
@@ -1,5 +1,23 @@
-
 import { parser, collector } from './exports';
+
+
+/* Вспомогательная функция складывающая 2 распаршенных одночлена
+  Сигнатура:
+    monSet1 - Object -> { variables: String, coefficient: Number, powers: { key_n:  Number_n } }
+    monSet2 - Object -> { variables: String, coefficient: Number, powers: { key_n:  Number_n } }
+*/
+const mergeTwoMonomsSet = (monSet1, monSet2) => ({
+  variables: monSet1.variables,
+  coefficient: (monSet1.coefficient + monSet2.coefficient),
+  powers: monSet1.variables
+    .reduce((powers, varbl) => {
+        powers[varbl] = Math.max(
+          monSet1.powers[varbl],
+          monSet2.powers[varbl]
+         );
+      return powers;
+    }, {})
+})
 
 
 /* Алгебраический сумматор, полиномов.
@@ -7,18 +25,24 @@ import { parser, collector } from './exports';
      polynom1 - String -> Полином записанный в форме строки.
      polynom2 - String -> Полином записанный в форме строки.
      sum - Boolean -> Флаг, по умолчанию: (true)-складывать, (false)-вычитать.
+
+     return: 
 */
-const algSumator = (polynom1, polynom2, sum = true) => {
-    const pMap1 = parser(polynom1);
-    const pMap2 = parser(polynom2);
-    const mainPolyMap = new Object(pMap1);
-  
-    for ([key, value] of Object.entries(pMap2)) {
-      if (mainPolyMap[key]) mainPolyMap[key] += sum ? value : (value * -1);
-      else mainPolyMap[key] = value;
-    }
-    return collector(mainPolyMap);
-  
+const polynomAlgSumator = (polynom1, polynom2, sum = true) => {
+  const monomList1 = polyParser(polynom1);
+  const monomList2 = polyParser(polynom2);
+  const mainMonomsList = [...monomList1, ...monomList2];
+
+  const algSumOfMonoms = mainMonomsList.reduce((acc, monomSet) => {
+    const variables = monomSet.variables.join(' ');
+    if (acc[variables])
+      acc[variables] = mergeTwoMonomsSet(acc[variables], monomSet)
+    else acc[variables] = monomSet;
+
+    return acc;
+  }, {});
+
+  return algSumOfMonoms;  //constructPolynom
 }
 
 export { algSumator };
