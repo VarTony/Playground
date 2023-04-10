fc7f6599e7d1d0d6e9126707373f1bfa323cd167
Anion <littlesalo3@gmail.com>
2023-04-10 13:58:42 +0300

    Added new powerHandler for polyParser

--- JS/PURE_JS/MATH/Algebra/Polynomials/Parsers/polynomParser.js
+++ JS/PURE_JS/MATH/Algebra/Polynomials/Parsers/polynomParser.js
@@ -1,6 +1,24 @@
 import { monomParser } from "./exports";
 
 
+/* Вспомогательная функция для обработки одночленом, перед отправкой в парсер
+    Сигнатура:
+     monom - String -> Полином в виде строки
+
+    return - String -> Одночлен с возведенным в свою степень коэффициентом
+*/
+const powerHandler = monom => {
+    if (/\d+\^\d+/.test(monom)) {
+      const splited = monom.split('^');
+      const base = splited[0];
+      const power = splited[1];
+  
+      return String((+base) ** (+power));
+    }
+    return monom;
+  }
+
+
 /*  Парсер для полиномов первой степени.
     Сигнатура:
      polynom - String -> Полином в виде строки
@@ -10,7 +28,7 @@ const polyParser = polynom => {
     const monomsList = neatPolynom
      .replace(/(?<!\^)([\+\-]{1})/g, ' $1') // Находит знак алгебраической суммы, не являющийся знаком степени/
      .split(' ');
-    const ParsedMonomsList = monomsList.map(monom => monomParser(monom));
+    const ParsedMonomsList = monomsList.map(monom => monomParser(powerHandler(monom)));
   
     return ParsedMonomsList;
   }
