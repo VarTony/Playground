5461d8d3a81bea72cce2490e47709ebf031bef32
VarTony <littlesalo3@gmail.com>
2023-05-12 08:08:33 +0200

    174.1

--- JS/typescript/patterns/Behavioral/Observer.ts
+++ JS/typescript/patterns/Behavioral/Observer.ts
@@ -1,5 +1,15 @@
-// Observer
+/**
+ * Паттерн Наблюдатель - позволяет подвязать наблюдателей к какому-либо классу, реализуещему 
+ *  интерфейс оповещателя ('субьекта'), и получать от него рассылку при обновление данных, при
+ *  этом связи между объектами являются 'слабыми', не важно какого типа получатели, а какого 
+ *  отправитель, важно лишь то что они имлементируют интерфейсы 'Субъекта и наблюдателя', что 
+ *  хорошо сказывается на гибкости и масштабируемости программы.
+ */
 
+
+/**
+ * Блок субъекта:
+ */
 interface ISubject {
     attach(observer: IObserver): void;
     detach(observer: IObserver): void;
@@ -7,7 +17,7 @@ interface ISubject {
     getData(): any;
     setData(data: any): void;
   }
-  
+   
   
   class SomebodySubject implements ISubject {
     private observers: IObserver[] = [];
@@ -21,7 +31,7 @@ interface ISubject {
     public detach(observer: IObserver): void {
       const observIndx = this.observers.indexOf(observer);
       this.observers = this.observers
-        .filter((observer, i) => i !== observIndx);
+        .filter((_, i) => i !== observIndx);
     }
 
     public notify() {
@@ -37,8 +47,11 @@ interface ISubject {
     }
   }
   
+
   
-  
+  /**
+   * Блок наблюдателя:
+   */
   interface IObserver {
     update(subject: ISubject, index: number): void;
   }
@@ -52,16 +65,19 @@ interface ISubject {
       this.displayUpdate(i);
     }
   
-    /** В данном случае метод отображения вызывается в методе обновления 
-    *   для большей наглядности, но лучше так не делать. 
-    */ 
+     /** 
+     * В данном случае метод отображения вызывается в методе обновления 
+     *   для большей наглядности, но лучше так не делать. 
+     */ 
     private displayUpdate(i: number) {
       console.log(this.updatedData, `(Index of observer: ${i})`);
     }
   }
   
   
-  
+   /**
+   * Экземпляры и тестирование 
+   */
   const somebodySubject_1 = new SomebodySubject();
   const somebodyObserver_1 = new SomebodyObserver();
   const somebodyObserver_2 = new SomebodyObserver();
@@ -69,5 +85,7 @@ interface ISubject {
   somebodySubject_1.attach(somebodyObserver_1);
   somebodySubject_1.attach(somebodyObserver_2);
   somebodySubject_1.setData('Something data');
+  somebodySubject_1.notify(); // Something data (Index of observer: 0) ... & (Index of observer: 1)
   somebodySubject_1.detach(somebodyObserver_2);
   somebodySubject_1.setData('Only for you <3');
+  somebodySubject_1.notify(); // Only for you <3 (Index of observer: 0)
