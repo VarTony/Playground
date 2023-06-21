5dd7d96770c994e71f4b7e4a361b7a9c7f93a4d6
VarTony <littlesalo3@gmail.com>
2023-06-21 14:21:15 +0200

    Writen main logic for then method

--- JS/NODEJS/Fundamental/Abstraction/Custom_promise/Custom_promise.js
+++ JS/NODEJS/Fundamental/Abstraction/Custom_promise/Custom_promise.js
@@ -1,23 +1,111 @@
-class CustomPromise {
-    constructor(executor){
-        executor(value => { this.hidden = value });
-        this.callbacks = [];
-    }
+// Реализация кастомной 'лайт' версии промиса, для лучшего понимания его функционирования;
 
-    resolve() {
-        const [ cb, ...cbs ] = this.callbacks;
-        let result = cb(this.hidden);
-        cbs.forEach(cb => {  result = cb(result) });
 
-        return result;
-    }
-
-    then(cb) {
-        this.callbacks.push(cb);
-        this.resolve();
-
-        return this;
-    }
-
-    catch(cb) {}
+const STATE = {
+  PENDING: 'PENDING',
+  FULFILLED: 'FULFILLED',
+  REJECTED: 'REJECTED',
 }
+
+
+
+class CustomPromise {
+    constructor(executor) {
+      this.callbacks = [];
+      this.executorState = STATE.PENDING;
+      this.cbState = STATE.PENDING;
+
+      executor(this.resolve.bind(this));
+    }
+  
+
+    /**
+     * Определяет является ли функция ассинхронной
+     * 
+     * @param { Function } cb 
+     * @returns 
+     */
+    _isAsyncCallback(cb) {
+        return cb.constructor.name === 'AsyncFunction';
+    }
+
+
+    /**
+     * Создает отложеный вызов в начале следующего цикла петли событий.
+     */
+    _callbacksDispather(data = this.promiseData) {
+      process.nextTick(() => { this._queueResolver(data) });
+    }
+    
+
+    /**
+     * Проверяет завершеность исполнения executor(a),
+     *  если видит состояние ожидания, создает запланированный самовызов
+     *  в начале слудующего цикла event loop, если состояние 'FULFILLED',
+     *  вызывает диспетчера очереди обратных вызовов.
+     * 
+     * @returns 
+     */
+    _observer() {
+      return executorState !== STATE.FULFILLED
+        ? process.nextTick(() => this._observer())
+        : this._callbacksDispather();
+    }
+
+
+    /**
+     * Записывает результат выполнения executor(a) и 
+     * иницирует запуск диспечера очереди колбеков.
+     * 
+     * @param {*} data 
+     */
+    resolve(data) {
+      this.promiseData = typeof data === 'object' ? { ...data } : data; 
+      this.executorState = STATE.FULFILLED;
+      this._callbacksDispather();
+    }
+
+
+    /**
+     * Обработывает по цепочке в хронологическом порядке переданные функции обратного вызова.
+     * 
+     * @param {*} data 
+     * @returns 
+     */
+    _queueResolver(data) {
+      if (!this.callbacks.length) {
+        this.cbState = STATE.FULFILLED;
+        return;
+      }
+      const [ cb, ...cbs ] = this.callbacks;
+    
+      if (this._isAsyncCallback(cb)) {
+        cb(data).then(result => { 
+          this.callbacks = cbs;
+          this.queueResolver(result);
+      });
+      // Если callback является асинхронным создает отложенный рекурсивный вызов, 
+      // через метод посредник.
+      return this._callbacksDispather(data);
+      }
+      else { 
+        this.callbacks = cbs;
+        return this.queueResolver(cb(data));
+      }
+    }
+  
+
+    /**
+     * Метод добавляет функцию обратного вызова в список колбеков
+     * и запускает наблюдателя для дальнейшией обработки формируемой очереди.
+     * 
+     * @param {Function} cb 
+     * @returns 
+     */
+    then(cb) {
+      this.callbacks.push(cb);
+      this.cbState = STATE.PENDING;
+      this._observer();
+      return this;
+    }
+  }
