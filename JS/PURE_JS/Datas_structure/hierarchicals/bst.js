a4966b73f32b324926720684d5f8aec2bc0395b2
VarTony <littlesalo3@gmail.com>
2023-09-01 21:33:56 +0200

    190

--- JS/PURE_JS/Datas_structure/hierarchicals/bst.js
+++ JS/PURE_JS/Datas_structure/hierarchicals/bst.js
@@ -6,7 +6,7 @@
  *   left - узел, в качестве данных которого меньшее значение, чем у родительского узла.
  *   right - узел, в качестве данных которого меньшее значение, чем у родительского узла.
  * 
- *    Данная реализация имеет несколько процедур для своего обслуживания:
+ *    Данная реализация имеет несколько процедур для  самообслуживания:
  *      setData(num: number) - Добавление нового элемента в дерево.(Л-Рекурсия)
  *      getData(num: number) - Поиск и проверка содержания заданного элемента.(Л-Рекурсия)
  *      getBiggest(void) - Нахождение наибольшего элемента в структуре.(Л-Рекурсия)
@@ -14,6 +14,9 @@
  *      inOrder(void) - Возвращает отсортированный в порядке возрастания 
  *          список всех элементов дерева.(К-Рекурсия)
  *      getTree(void) - Возвращает полную копию дерева, в его текущем состоянии.
+ *      getAllBranches(void) - Нахождение всех ветвей дерева(От корня до листового узла).(К-Рекурсия)
+        getDeep(void) - Нахождение глубины дерева(Колличество узлов самой длиной ветви).
+ * 
  */
 const BST = () => {
     const tree = {};
@@ -38,7 +41,28 @@ const BST = () => {
       if(t.data > num) return getData(num, t.left);
     }
     
+
+    const _isEmptyNode = node => node.data === undefined;
+
+
+    const _getAllBranches = (bst, path = []) => {
+      if (_isEmptyNode(bst)) return [];
+      const road = [ ...path, bst.data ];
+      if (_isEmptyNode(bst.left) && _isEmptyNode(bst.right)) return [ road ];
   
+      return [ 
+          ..._getAllBranches(bst.left, road),
+          ..._getAllBranches(bst.right, road)
+      ]
+    }
+
+    const getAllBranches = () => _getAllBranches(tree);
+
+
+    const getDeep = () => getAllBranches()
+      .reduce((deep, branch) => branch.length > deep ? branch.length : deep, 0);
+  
+
     const getBiggest = (t=tree) => (t.right.data !== undefined)
       ? getBiggest(t.right)
       : t.data;
@@ -69,7 +93,9 @@ const BST = () => {
       getTree,
       getBiggest,
       getSmaller,
-      inOrder
+      inOrder,
+      getAllBranches,
+      getDeep
     }
   }
   
