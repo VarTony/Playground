15ca04707d5ddb9dcfd43cc52020c50fb458cc95
VarTony <littlesalo3@gmail.com>
2023-12-03 04:05:11 +0100

    193.1

--- Python/Neural neworks/BASE/ConcreteFormulas/KNNImplementations.py
+++ Python/Neural neworks/BASE/ConcreteFormulas/KNNImplementations.py
@@ -1,6 +1,17 @@
 import math
 
 
+# Вспомогательные функции
+
+# Сумма квадратов значений из переданного списка
+def sumOfSquares(numList):
+    return sum([(x ** 2) for x in numList])
+
+# Квадратный корень
+def sqrt(num):
+    return num ** 0.5
+
+
 """
     Вычисляет Евклидово расстояние между двумя точками в n-мерном пространстве.
      point_1 и point_2 должны быть итерируемыми объектами (например, списками) одинаковой длины.
@@ -34,6 +45,24 @@ def manhattanDistance(point_1, point_2):
 
 
 """
+   Минковского расстояние (Minkowski Distance): Обобщение Евклидова и Манхэттенского расстояний.
+    Евклидово расстояние — это частный случай Минковского расстояния при p=2, Манхэттенское — при p=1.
+    Оно может быть адаптировано для разных значений p.
+
+    Minkowski Distance = (Σ|p_i - q_i|^p)^(1/p) for i in 1 to n, где
+    p_i и q_i - соответствующие координаты точек point_1 и point_2,
+    p - параметр, определяющий степень (по умолчанию p=2, Евклидово расстояние).
+"""
+
+def minkowskiDistance(point_1, point_2, p=2):
+    distance = 0
+    for i in range(len(point_1)):
+        distance += (point_1[i] - point_2[i]) ** p
+
+    return distance ** (1/p)
+
+
+"""
     Чебышёвское расстояние (Chebyshev Distance): Максимум абсолютных разностей между 
      соответствующими координатами двух точек. Используется, например, в шахматах 
      для определения минимального количества ходов, необходимых королю, чтобы добраться 
@@ -41,6 +70,7 @@ def manhattanDistance(point_1, point_2):
 
     Chebyshev Distance = max[i] |p_i - q_i| for i in 1 to n
 """
+
 def chebyshevDistance(point_1, point_2):
     distances = [abs(p - q) for p, q in zip(point_1, point_2)]
     return max(distances)
@@ -55,3 +85,18 @@ def chebyshevDistanceAltr(point_1, point_2):
     return max(pairs)
 
 
+"""
+    Косинусное сходство (Cosine Similarity): Мера сходства между двумя векторами в 
+     многомерном пространстве, основанная на косинусе угла между этими векторами. 
+     Используется в различных областях, включая информационный поиск и обработку естественного языка, 
+     для определения степени сходства между двумя векторами.
+
+    Cosine Similarity = (Σ p_i * q_i) / (sqrt(Σ p_i^2) * sqrt(Σ q_i^2))
+     где p_i и q_i - соответствующие элементы векторов point_1 и point_2.
+"""
+
+def cosineSimilarity(point_1, point_2):
+    numerator = sum([(p * q) for p, q in zip(point_1, point_2)])
+    denominator = sqrt(sumOfSquares(point_1)) * sqrt(sumOfSquares(point_2))
+
+    return numerator / denominator
