29f1fc7eac4b62f1961122070f6aba769b57e31b
VarTony <littlesalo3@gmail.com>
2024-03-23 13:39:04 +0300

    199

---
+++ JS/NODEJS/Some_functions/cp/cpByStream.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { pipeline } = require('node:stream/promises');
+const copyfile = process.argv[2];
+const copyToPath = process.argv[3];
+
+// Функция для копирования крупных файло, например для больших ISO образов
+const cpByStream = async (from, to ) => {
+    await pipeline(
+      fs.createReadStream(from),
+      fs.createWriteStream(to),
+    );
+    console.log('Pipeline succeeded.');
+}
+
+cpByStream(copyfile, copyToPath).catch(console.error);
